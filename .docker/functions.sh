76840613dae2e6e4370472f89dfd9495e356043d
Dan Webb <dan.webb@damacus.io>
2018-02-08 14:18:17 +0000

    Add context for circleci
    Remove unused variables

--- .docker/functions.sh
+++ .docker/functions.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 export PROJECT='docker-builder'
 export DESCRIPTION='Docker container with Docker, Inspec and Ruby'
-export MAINTAINER='damacus'
-export FILE='Dockerfile'
 
 if ! [[ -e .docker/external_functions.sh ]];then
   wget -O '.docker/external_functions.sh' 'https://raw.githubusercontent.com/damacus/docker-build-scripts/master/functions.sh'
