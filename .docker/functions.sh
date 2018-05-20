a8da8d5d243394a33a57b36f9bdbe761bec97808
Mike Cartwright <mcartwright-dbg@users.noreply.github.com>
2018-05-20 12:19:02 +0100

    Point to forked version

--- .docker/functions.sh
+++ .docker/functions.sh
@@ -2,7 +2,7 @@
 export DESCRIPTION='Docker container with Docker, Inspec and Ruby'
 
 if ! [[ -e .docker/external_functions.sh ]];then
-  wget -O '.docker/external_functions.sh' 'https://raw.githubusercontent.com/damacus/docker-build-scripts/master/functions.sh'
+  wget -O '.docker/external_functions.sh' 'https://raw.githubusercontent.com/deBankeGroup/docker-build-scripts/master/functions.sh'
 fi
 
 source '.docker/external_functions.sh'
